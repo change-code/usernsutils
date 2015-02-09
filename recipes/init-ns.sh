b6b486a99ea97e5fa4bb69e4ccb20c920b2c6106
bhuztez <bhuztez@gmail.com>
2015-02-09 17:30:33 +0800

    enable pty

--- recipes/init-ns.sh
+++ recipes/init-ns.sh
@@ -2,17 +2,22 @@
 
 mount -t proc proc /proc
 
-mount -B "${XDG_RUNTIME_DIR}/userns" /tmp
+mount -B "${XDG_RUNTIME_DIR}/userns" /mnt
 mount -t tmpfs tmpfs /run
 mkdir /run/userns
-mount -B /tmp /run/userns
-umount /tmp
+mount -M /mnt /run/userns
+
 mount -t tmpfs tmpfs /tmp
 
-mount -t tmpfs tmpfs /dev
-mkdir /dev/mqueue
 mount -t mqueue mqueue /dev/mqueue
-mkdir /dev/pts
-mount -t devpts -o newinstance,gid=0,mode=620 devpts /dev/pts
+mount -t devpts -o newinstance,gid=0,mode=600 devpts /dev/pts
+mount -B /dev/pts/ptmx /dev/ptmx
 
-env - LANG="${LANG}" PATH="${PATH}" HOME="${HOME}" SHELL="${SHELL}" TERM="${TERM}" USERNS_NAME="${USERNS_NAME}" XDG_RUNTIME_DIR=/run "$@"
+SRCDIR=$(readlink -f "$(dirname ${BASH_SOURCE})")
+ETC_FILES="passwd group"
+
+for filename in ${ETC_FILES}; do
+    mount -B "${SRCDIR}/${filename}" "/etc/${filename}"
+done
+
+exec env - LANG="${LANG}" PATH="${PATH}" HOME="${HOME}" SHELL="${SHELL}" TERM="${TERM}" USERNS_NAME="${USERNS_NAME}" XDG_RUNTIME_DIR=/run "$@"
--- src/listen.c
+++ src/listen.c
@@ -16,87 +16,6 @@ static void show_usage() {
   exit(0);
 }
 
-
-static int handle_connection(int fd, char *const argv[]) {
-  int slave = recv_fd(fd);
-  close(fd);
-
-  int ttyfd = open("/dev/tty", O_RDWR | O_NOCTTY);
-  if (ttyfd != -1) {
-    PERROR(==-1, ioctl, ttyfd, TIOCNOTTY);
-    close(ttyfd);
-  }
-
-  PERROR(==-1, setsid);
-  PERROR(==-1, ioctl, slave, TIOCSCTTY);
-
-  pid_t pid;
-  PERROR(==-1, pid = fork);
-
-  if (pid > 0) {
-    VERBOSE("SPAWN pid=%d\n", pid);
-    close(slave);
-
-    for(;;) {
-      int status = -1;
-      pid_t child_pid = waitpid(pid, &status, 0);
-
-      if (WIFSTOPPED(status)) {
-        continue;
-      }
-
-      int code = 1;
-
-      if(WIFSIGNALED(status)) {
-        code = WTERMSIG(status)+128;
-      } else {
-        code = WEXITSTATUS(status);
-      }
-
-      VERBOSE("EXIT pid=%d status=%d\n", child_pid, code);
-      return code;
-    }
-    return 0;
-  }
-
-  int close_fds[3] = {STDIN_FILENO, STDOUT_FILENO, STDERR_FILENO};
-
-  for(size_t i=0; i<(sizeof(close_fds)/sizeof(int)); i++) {
-    close(close_fds[i]);
-    dup2(slave, close_fds[i]);
-  }
-  close(slave);
-
-  PERROR(==-1, execvp, argv[0], argv);
-  return EXIT_FAILURE;
-}
-
-
-static int do_listen(int listen_fd, char *const argv[]) {
-  PERROR(==-1, listen, listen_fd, SOMAXCONN);
-
-  for(;;) {
-    int fd = -1;
-
-    struct sockaddr_un peer;
-    socklen_t size = sizeof(struct sockaddr_un);
-    pid_t pid;
-
-    PERROR(==-1, fd = accept, listen_fd, &peer, &size);
-    PERROR(==-1, pid = fork);
-
-    if (pid == 0) {
-      close(listen_fd);
-      return handle_connection(fd, argv);
-    }
-
-    close(fd);
-  }
-
-  return 0;
-}
-
-
 int cmd_listen(int argc, char *const argv[]) {
   int opt, index;
 
@@ -120,19 +39,19 @@ int cmd_listen(int argc, char *const argv[]) {
   char *name = getenv("USERNS_NAME");
   ERROR(!name, "running outside a user namespace\n");
 
-  char socket_path[PATH_MAX] = {0};
-  snprintf(socket_path, PATH_MAX, "%s/userns/%s/nshd", rundir, name);
-  unlink(socket_path);
+  char arg_unix_listen[PATH_MAX+17] = {0};
+  PERROR(<0, snprintf, arg_unix_listen, sizeof(arg_unix_listen), "UNIX-LISTEN:%s/userns/%s/telnetd,fork", rundir, name);
 
-  int listen_fd = -1;
-  PERROR(==-1, listen_fd = socket, AF_UNIX, SOCK_STREAM, 0);
+  char *command = (optind >= argc)?getenv("SHELL"):argv[optind];
+  if (!command) {
+    command = "/bin/sh";
+  }
 
-  struct sockaddr_un addr = {.sun_family = AF_UNIX};
-  strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path)-1);
-  PERROR(==-1, bind, listen_fd, &addr, sizeof(addr));
+  char arg_exec[PATH_MAX+36] = {0};
+  PERROR(<0, snprintf, arg_exec, sizeof(arg_exec), "EXEC:%s,pty,setsid,setpgid,stderr,ctty", command);
 
-  VERBOSE("start listening on '%s'\n", socket_path);
-  return do_listen(listen_fd, make_argv(optind, argc, argv));
+  PERROR(==-1, execlp, "socat", "socat", arg_unix_listen, arg_exec, NULL);
+  exit(EXIT_FAILURE);
 err:
   fprintf(stderr, "Try '%s %s --help'\n", executable, cmd_name);
   exit(EXIT_FAILURE);
